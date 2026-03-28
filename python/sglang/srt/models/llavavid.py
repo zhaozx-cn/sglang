bb638408568747c8d2b7916509e92a950ec913bd
zhaozixin <zhaozixin1@huawei.com>
2026-03-28 13:05:08 +0800

    fix llava next video
    Signed-off-by: zhaozixin <zhaozixin1@huawei.com>

--- python/sglang/srt/models/llavavid.py
+++ python/sglang/srt/models/llavavid.py
@@ -135,7 +135,7 @@ class LlavaVidForCausalLM(nn.Module):
             if need_vision.any():
                 pixel_values = flatten_nested_list(
                     [
-                        [item.feature for item in image_inputs[i].mm_items]
+                        [image_inputs[i].image_offsets]
                         for i in range(bs)
                         if need_vision[i]
                     ]
