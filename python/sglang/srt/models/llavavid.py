a67508eab1136c924f57e0a440ca75889bc920cf
zhaozixin <zhaozixin1@huawei.com>
2026-03-30 15:00:28 +0800

    fix copy error
    Signed-off-by: zhaozixin <zhaozixin1@huawei.com>

--- python/sglang/srt/models/llavavid.py
+++ python/sglang/srt/models/llavavid.py
@@ -135,14 +135,14 @@ class LlavaVidForCausalLM(nn.Module):
             if need_vision.any():
                 pixel_values = flatten_nested_list(
                     [
-                        [image_inputs[i].image_offsets]
+                        [item.feature for item in image_inputs[i].mm_items]
                         for i in range(bs)
                         if need_vision[i]
                     ]
                 )
                 image_offsets = [
                     flatten_nested_list(
-                        [item.offsets for item in image_inputs[i].mm_items]
+                        [image_inputs[i].image_offsets]
                     )
                     for i in range(bs)
                     if need_vision[i]
