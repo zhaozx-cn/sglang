b84c818a9c737e4c89f4a5c0588f82a357cc45a4
zhaozixin <zhaozixin1@huawei.com>
2026-04-23 14:51:02 +0800

    chunk o update
    Signed-off-by: zhaozixin <zhaozixin1@huawei.com>

--- python/sglang/srt/hardware_backend/npu/attention/fla/chunk_o_update.py
+++ python/sglang/srt/hardware_backend/npu/attention/fla/chunk_o_update.py
@@ -1,34 +1,22 @@
-from typing import List, Optional, Tuple, Union
-
 import torch
-import torch.nn.functional as F
 import triton
 import triton.language as tl
-from sglang.srt.hardware_backend.npu.attention.fla.utils import (
-    exp,
-    prepare_chunk_indices,
-    prepare_chunk_offsets,
-    safe_exp,
-)
+
+from sglang.srt.hardware_backend.npu.attention.fla.utils import prepare_chunk_offsets
 
 
 @triton.heuristics(
     {
-        "USE_G": lambda args: args["g"] is not None,
         "IS_VARLEN": lambda args: args["cu_seqlens"] is not None,
     }
 )
 @triton.jit(do_not_specialize=["T"])
-def chunk_fwd_kernel_o_npu_kernel(
-    q,
-    k,
-    v,
+def chunk_fwd_kernel_o_update(
     h,
-    g,
-    o,
+    h_update,
+    updated_h_state,
     cu_seqlens,
     chunk_offsets,
-    scale,
     T,
     H: tl.constexpr,
     Hg: tl.constexpr,
@@ -37,17 +25,13 @@ def chunk_fwd_kernel_o_npu_kernel(
     BT: tl.constexpr,
     BK: tl.constexpr,
     BV: tl.constexpr,
-    USE_G: tl.constexpr,
     IS_VARLEN: tl.constexpr,
 ):
     i_v, i_nh = tl.program_id(0), tl.program_id(1)
-    i_n, i_h = i_nh // H, i_nh % H
-    T_max = T
+    i_n, i_h = i_nh // H, i_nh % H  # splitting by the head of the req
 
     if IS_VARLEN:
-        bos, eos = tl.load(cu_seqlens + i_n).to(tl.int32), tl.load(
-            cu_seqlens + i_n + 1
-        ).to(tl.int32)
+        bos, eos = tl.load(cu_seqlens + i_n).to(tl.int32), tl.load(cu_seqlens + i_n + 1).to(tl.int32)
         T = eos - bos
         NT = tl.cdiv(T, BT)
         boh = tl.load(chunk_offsets + i_n).to(tl.int64)
@@ -57,90 +41,44 @@ def chunk_fwd_kernel_o_npu_kernel(
         boh = i_n * NT
 
     # offset calculation
-    q += (bos * Hg + i_h // (H // Hg)) * K
-    k += (bos * Hg + i_h // (H // Hg)) * K
-    v += (bos * H + i_h) * V
-    o += (bos * H + i_h) * V
+    updated_h_state += (i_n * H + i_h).to(tl.int64) * K * V
 
     for i_t in range(NT):
         i_tg = boh + i_t
         h_base = h + (i_tg * H + i_h).to(tl.int64) * K * V
-        b_o = tl.zeros([BT, BV], dtype=tl.float32)
-        b_A = tl.zeros([BT, BT], dtype=tl.float32)
+        hupd_base = h_update + ((i_tg + i_n) * H + i_h).to(tl.int64) * K * K
+
         for i_k in range(tl.cdiv(K, BK)):
-            p_q = tl.make_block_ptr(
-                q, (T, K), (Hg * K, 1), (i_t * BT, i_k * BK), (BT, BK), (1, 0)
+            p_h = tl.make_block_ptr(h_base, (K, V), (V, 1), (i_k * BK, i_v * BV), (BK, BV), (1, 0))
+            p_hupd = tl.make_block_ptr(hupd_base, (K, V), (V, 1), (i_k * BK, i_v * BV), (BK, BK), (1, 0))
+            p_updated_h_state = tl.make_block_ptr(
+                updated_h_state, (K, V), (V, 1), (i_k * BK, i_v * BV), (BK, BV), (1, 0)
             )
-            # [BT, BK]
-            b_q = tl.load(p_q, boundary_check=(0, 1), padding_option="zero")
 
-            p_k = tl.make_block_ptr(
-                k, (K, T), (1, Hg * K), (i_k * BK, i_t * BT), (BK, BT), (0, 1)
-            )
-            # [BK, BT]
-            b_k = tl.load(p_k, boundary_check=(0, 1), padding_option="zero")
-
-            p_h = tl.make_block_ptr(
-                h_base, (K, V), (V, 1), (i_k * BK, i_v * BV), (BK, BV), (1, 0)
-            )
             # [BK, BV]
-            b_h = tl.load(p_h, boundary_check=(0, 1), padding_option="zero")
+            b_h = tl.load(p_h, boundary_check=(0, 1))
+            # [BK, BK]
+            b_hupd = tl.load(p_hupd, boundary_check=(0, 1))
+            # [BK, BV]
+            b_updated_h_state = tl.load(p_updated_h_state, boundary_check=(0, 1))
 
-            # [BT, BK] @ [BK, BV] -> [BT, BV]
-            b_o += tl.dot(b_q, b_h)
-            # [BT, BK] @ [BK, BT] -> [BT, BT]
-            b_A += tl.dot(b_q, b_k)
-
-        if USE_G:
-            offs_t = i_t * BT + tl.arange(0, BT)
-            mask_t = offs_t < T
-            g_ptr = g + bos + i_h * T_max
-            b_g = tl.load(g_ptr + offs_t, mask=mask_t, other=0.0)
-
-            b_o = b_o * tl.exp(b_g)[:, None]
-            b_A = b_A * safe_exp(b_g[:, None] - b_g[None, :])
-
-        o_i = tl.arange(0, BT).to(tl.float32)
-        m_A = o_i[:, None] >= o_i[None, :]
-        b_A = tl.where(m_A, b_A, 0)
-
-        p_v = tl.make_block_ptr(
-            v, (T, V), (H * V, 1), (i_t * BT, i_v * BV), (BT, BV), (1, 0)
-        )
-        p_o = tl.make_block_ptr(
-            o, (T, V), (H * V, 1), (i_t * BT, i_v * BV), (BT, BV), (1, 0)
-        )
-        b_v = tl.load(p_v, boundary_check=(0, 1), padding_option="zero")
-
-        # to fix mma -> mma layout conversion
-        # already solved by triton v3.2 or higher
-        b_o = b_o * scale + tl.dot(b_A.to(b_v.dtype), b_v) * scale
-
-        tl.store(p_o, b_o.to(p_o.dtype.element_ty), boundary_check=(0, 1))
+            b_h += tl.dot(b_hupd.to(tl.bfloat16), b_updated_h_state.to(tl.bfloat16))
+            tl.store(p_h, b_h.to(p_h.dtype.element_ty), boundary_check=(0, 1))
 
 
-def chunk_fwd_o_npu(
+def chunk_fwd_o_update(
     q: torch.Tensor,
-    k: torch.Tensor,
     v: torch.Tensor,
     h: torch.Tensor,
-    g: Optional[torch.Tensor] = None,  # cumsum of log decay
-    scale: Optional[float] = None,
-    cu_seqlens: Optional[torch.LongTensor] = None,
+    h_update: torch.Tensor,
+    updated_h_state: torch.Tensor,
+    cu_seqlens: torch.LongTensor | None = None,
     chunk_size: int = 64,
 ) -> torch.Tensor:
     B, T, Hg, K, V = *q.shape, v.shape[-1]
     H = v.shape[-2]
-    # BT = min(chunk_size, max(16, triton.next_power_of_2(T)))
     BT = chunk_size
-    chunk_indices = (
-        prepare_chunk_indices(cu_seqlens, BT) if cu_seqlens is not None else None
-    )
-    NT = triton.cdiv(T, BT) if cu_seqlens is None else len(chunk_indices)
-    if scale is None:
-        scale = k.shape[-1] ** -0.5
 
-    o = torch.empty_like(v)
     if cu_seqlens is None:
         N, chunk_offsets = B, None
     else:
@@ -152,19 +90,12 @@ def chunk_fwd_o_npu(
     def grid(meta):
         return (triton.cdiv(V, meta["BV"]), N * H)
 
-    # Pre-transpose tensors outside the kernel to ensure contiguous memory access within the kernel
-    # avoiding scattered (non-contiguous) access that may lead to axis expansion
-    g = g.transpose(1, 2).contiguous()
-    chunk_fwd_kernel_o_npu_kernel[grid](
-        q,
-        k,
-        v,
-        h,
-        g,
-        o,
-        cu_seqlens,
-        chunk_offsets,
-        scale,
+    chunk_fwd_kernel_o_update[grid](
+        h=h,
+        h_update=h_update,
+        updated_h_state=updated_h_state,
+        cu_seqlens=cu_seqlens,
+        chunk_offsets=chunk_offsets,
         T=T,
         H=H,
         Hg=Hg,
@@ -176,4 +107,4 @@ def chunk_fwd_o_npu(
         num_warps=4,
         num_stages=2,
     )
-    return o
+    return h
